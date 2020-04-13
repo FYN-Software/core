b29ba3f62b190141e0e6a1a2e676ee3df59e16d9
Chris Kruining <c.kruining@fyn.nl>
2020-04-13 18:38:35 +0200

    switched cdn

--- driver/idb.js
+++ driver/idb.js
@@ -1,4 +1,4 @@
-import * as Comlink from 'https://unpkg.com/comlink/dist/esm/comlink.mjs';
+import * as Comlink from 'https://cdn.jsdelivr.net/npm/comlink@4.2.0/dist/esm/comlink.min.mjs';
 
 export default class Idb
 {

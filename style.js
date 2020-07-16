52658190dbc9721f6823973f86c87cfefa7ff845
Chris Kruining <c.kruining@fyn.nl>
2020-07-16 16:27:04 +0200

    implemented workaround due to deprecation

--- style.js
+++ style.js
@@ -15,14 +15,28 @@ export default class Style
         });
     }
 
-    static set(key, url, options = {})
+    static async set(key, url, options = {})
     {
         if(this.#map.has(key) === false)
         {
             this.#map.set(key, new CSSStyleSheet({ crossOrigin: 'anonymous' }));
         }
 
-        fetch(`${url}?fyn.core.style`, options).then(r => r.text()).then(r => this.#map.get(key).replace(r));
+        let css = await fetch(`${url}?fyn.core.style`, options).then(r => r.text());
+
+        // NOTE(Chris Kruining) Recursivly replace @import with the content. This has a 10 second timeout as backup exit condition for the while loop
+        let timedOut = false;
+        Promise.delay(10000).then(() => timedOut = true);
+
+        while(css.includes('@import') || timedOut)
+        {
+            css = await css.replaceAllAsync(
+                /@import url\('(https:\/\/[a-zA-Z0-9/:?#-_=~@%.|]+)'\)/gm,
+                (_, url) => fetch(`${url}?fyn.core.style`, options).then(r => r.text())
+            );
+        }
+
+        await this.#map.get(key).replace(css);
     }
 
     static fromString(key, content)
